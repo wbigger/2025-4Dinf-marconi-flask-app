3f0275fcbf3eb9dd4f929c284f8668c0a12827fb
claudio.capobianco <claudio.capobianco@marconicloud.it>
2025-05-15 08:03:15 +0000

    Add dynamic index

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import os
 
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='src')
+
+
+posts=[
+    {'title': "Primo post", 'content': "Questo è il contenuto del primo post."},
+    {'title': "Secondo post", 'content': "Questo è il contenuto del secondo post."},
+    {'title': "Terzo post", 'content': "Questo è il contenuto del terzo post."}
+]
 
 @app.route("/")
 def index():
-    return send_file('src/index.html')
+    return render_template('index.html', posts=posts)
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
